f6ea5c13c3358295ef5f64e7c37b602b8900a412
crismblog <social@crism.ro>
2022-02-13 22:35:54 +0200

    update

--- .startup_conky.sh
+++ .startup_conky.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 15
-conky -c /home/cristian/.config/conky/conkyrc1 &
-conky -c /home/cristian/.config/conky/conkyrc2 & exit
+conky -c /home/user_name.config/conky/conkyrc1 &
+conky -c /home/user_name/.config/conky/conkyrc2 & exit
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # conky
+
+1. In the .startup_conky.sh file, the "user_name" must be replaced with your username.
+2. The conky directory must be moved to the .config directory.
